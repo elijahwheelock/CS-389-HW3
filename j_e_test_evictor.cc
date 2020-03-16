494177db7301119837925645461e98c53510c043
Jonah <jonahkohn@gmail.com>
2020-03-15 20:13:35 -0700

    Added extra evictor tests.

--- j_e_test_evictor.cc
+++ j_e_test_evictor.cc
@@ -5,7 +5,6 @@
 #include "fifo_evictor.hh"
 #include "catch2.hh"
 
-
 void reset() {
   while (e.evict() != "") {}
 }
@@ -16,7 +15,7 @@ TEST_CASE("Evictor works as described in fifo_evictor.hh", "[evictor]") {
   reset();
   REQUIRE(e.evict() == "");
 
-  SECTION("basic functions"){
+  SECTION("Evicting after touching a key should return that key."){
     e.touch_key("hello");
     REQUIRE(e.evict() == "hello");
 
@@ -26,8 +25,7 @@ TEST_CASE("Evictor works as described in fifo_evictor.hh", "[evictor]") {
     REQUIRE(e.evict() == "");
   }
 
-
-  SECTION("Ordered insertion and eviction, with duplicates"){
+  SECTION("Insertion order should be respected in eviction order, even with duplicates."){
     reset();
     REQUIRE(e.evict() == "");
 
@@ -44,5 +42,29 @@ TEST_CASE("Evictor works as described in fifo_evictor.hh", "[evictor]") {
     REQUIRE(e.evict() == "2");
     REQUIRE(e.evict() == "1");
     REQUIRE(e.evict() == "4");
+    REQUIRE(e.evict() == "");
+
+  }
+
+  SECTION("Emptying the cache between insertions should not change behavior."){
+
+    e.touch_key("2");
+    e.touch_key("3");
+    e.touch_key("2");
+
+    REQUIRE(e.evict() == "2");
+    REQUIRE(e.evict() == "3");
+    REQUIRE(e.evict() == "2");
+    REQUIRE(e.evict() == "");
+
+    e.touch_key("2");
+    e.touch_key("1");
+    e.touch_key("4");
+
+    REQUIRE(e.evict() == "2");
+    REQUIRE(e.evict() == "1");
+    REQUIRE(e.evict() == "4");
+
+    REQUIRE(e.evict() == "");
   }
 }
